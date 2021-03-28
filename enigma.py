461d66af23f197a6396908f2b30868057ac2278a
Callum Skeet <callum@elitemail.org>
2021-03-28 21:39:13 +1100

    minor refactoring

--- enigma.py
+++ enigma.py
@@ -5,25 +5,28 @@ import database
 import string
 
 
-class enigma:
+class Enigma:
     """
     A simulated enigma machine that can encode and decode text
     """
+
     def __init__(self):
         """
         Ensures the Enigma machine has all settings
         configured for operation
         """
-        self.rotor_database = database.make_db('data', 'reflectors')
+        self.rotor_database = database.make_db("data", "reflectors")
         self.ab_list = list(string.ascii_uppercase)
 
-        print('Default Configuration:\n'
-              '\tModel: M3\n\tRotors (left-right): I, II, III\n'
-              '\tReflector: B\n\tRing Setting: AAA\n\tGround Setting: AAZ\n'
-              '\tPlugboard: None')
+        print(
+            "Default Configuration:\n"
+            "\tModel: M3\n\tRotors (left-right): I, II, III\n"
+            "\tReflector: B\n\tRing Setting: AAA\n\tGround Setting: AAZ\n"
+            "\tPlugboard: None"
+        )
 
-        setting = input('Customise configuration? (Y/N): ').lower()
-        if setting.startswith('y'):
+        setting = input("Customise configuration? (y/N): ").lower()
+        if setting.startswith("y"):
             self.model = self.select_model()
             self.rings = self.ring_settings()
             self.ground = self.ground_settings()
@@ -38,12 +41,16 @@ class enigma:
 
     def select_model(self, defaults=None):
         if defaults:
-            model = self.rotor_database['M3']
+            model = self.rotor_database["M3"]
         else:
             models = list(self.rotor_database.keys())
-            print('Select model: %s' % ', '.join(models))
-            model = self.rotor_database[input().title()]
-        return model
+            while True:
+                print("Select model: %s" % ", ".join(models))
+                selection = input().title()
+                if selection in self.rotor_database.keys():
+                    model = self.rotor_database[selection]
+                    return model
+                print('Model "%s" not found in database.' % selection)
 
     def ring_settings(self, defaults=None):
         """
@@ -52,33 +59,33 @@ class enigma:
         v: ABCDEFGHIJKLMNOPQRSTUVWXYZ,  ABCDEFGHIJKLMNOPQRSTUVWXYZ
         k: UWYGADFPVZBECKMTHXSLRINQOJ,  JUWYGADFPVZBECKMTHXSLRINQO
         """
-        if defaults:
-            l_ring, m_ring, r_ring = 0, 0, 0
-        else:
-            print('Input ring settings (A-Z):')
-            l_input = input('Left: ').upper()
-            l_ring = self.ab_list.index(l_input)
-
-            m_input = input('Middle: ').upper()
-            m_ring = self.ab_list.index(m_input)
-
-            r_input = input('Right: ').upper()
-            r_ring = self.ab_list.index(r_input)
-        return l_ring, m_ring, r_ring
+        ring_inputs = [0] * 3
+        if not defaults:
+            print("Input ring settings (A-Z):")
+            ring_inputs = [None] * 3
+            ring_names = ["Left", "Middle", "Right"]
+            for i, ring in enumerate(ring_names):
+                selection = ""
+                while not selection.isalpha():
+                    selection = input(f"{ring} : ").upper()
+                ring_inputs[i] = self.ab_list.index(selection)
+        return ring_inputs
 
     def ground_settings(self, defaults=None):
         """
         Sets the ground setting (or starting position)
         of the rotors e.g. AAZ, BFX
         """
-        if defaults:
-            left, middle, right = 0, 0, 25
-        else:
-            print('Input start positions (A-Z):')
-            left = self.ab_list.index(input('Left: ').upper())
-            middle = self.ab_list.index(input('Middle: ').upper())
-            right = self.ab_list.index(input('Right: ').upper())
-        return [left, middle, right]
+        start_positions = [0, 0, 25]
+        if not defaults:
+            print("Input start positions (A-Z):")
+            pos_names = ["left", "middle", "right"]
+            for i, pos in enumerate(pos_names):
+                selection = ""
+                while not selection.isalpha():
+                    selection = input(f"{pos.title()} : ").upper()
+                start_positions[i] = self.ab_list.index(selection)
+        return start_positions
 
     def reflected_path(self, rotor):
         """
@@ -95,41 +102,50 @@ class enigma:
         User selection of model, rotors and reflector
         """
         if defaults:
-            rotors = ((self.rotor_database['M3']['I'],
-                       self.reflected_path(self.rotor_database['M3']['I']),
-                       self.rotor_database['M3']['notches']['I']),
-                      (self.rotor_database['M3']['II'],
-                       self.reflected_path(self.rotor_database['M3']['II']),
-                       self.rotor_database['M3']['notches']['II']),
-                      (self.rotor_database['M3']['III'],
-                       self.reflected_path(self.rotor_database['M3']['III']),
-                       self.rotor_database['M3']['notches']['III']),
-                      self.rotor_database['M3']['reflectors']['B'])
+            rotors = (
+                (
+                    self.rotor_database["M3"]["I"],
+                    self.reflected_path(self.rotor_database["M3"]["I"]),
+                    self.rotor_database["M3"]["notches"]["I"],
+                ),
+                (
+                    self.rotor_database["M3"]["II"],
+                    self.reflected_path(self.rotor_database["M3"]["II"]),
+                    self.rotor_database["M3"]["notches"]["II"],
+                ),
+                (
+                    self.rotor_database["M3"]["III"],
+                    self.reflected_path(self.rotor_database["M3"]["III"]),
+                    self.rotor_database["M3"]["notches"]["III"],
+                ),
+                self.rotor_database["M3"]["reflectors"]["B"],
+            )
 
         else:
             rotors = self.model
-            reflectors = self.model['reflectors']
+            reflectors = self.model["reflectors"]
+            pos_names = ["left", "middle", "right"]
+            rotor_inputs = [None] * 3
+            choices = sorted(
+                [k for k in rotors.keys() if k not in ["notches", "reflectors"]]
+            )
+            print("Input rotor selection: " "%s" % ", ".join(choices))
+            for i, pos in enumerate(pos_names):
+                selection = ""
+                while not selection in rotors.keys():
+                    selection = input(f"{pos.title()} : ").upper()
+                rotor_inputs[i] = (rotors[selection],)
+                rotor_inputs[i] += (self.reflected_path(rotor_inputs[i][0]),)
+                if i > 0:
+                    rotor_inputs[i] += (rotors["notches"][selection],)
 
-            print('Input rotor selection: '
-                  '%s' % ', '.join(sorted(rotors.keys())))
-            l_input = input('Left: ').upper()
-            l_rotor = (rotors[l_input],)
-            l_rotor += (self.reflected_path(l_rotor[0]),)
+            print("Select a reflector: %s" % ", ".join(reflectors))
+            selection = ""
+            while not selection in reflectors:
+                selection = input("Reflector: ").upper()
+            reflector = reflectors[selection]
 
-            m_input = input('Middle: ').upper()
-            m_rotor = (rotors[m_input],)
-            m_rotor += (self.reflected_path(m_rotor[0]),)
-            m_rotor += (rotors['notches'][m_input],)
-
-            r_input = input('Right: ').upper()
-            r_rotor = (rotors[r_input],)
-            r_rotor += (self.reflected_path(r_rotor[0]),)
-            r_rotor += (rotors['notches'][r_input],)
-
-            print('Select a reflector: %s' % ', '.join(reflectors))
-            reflector = reflectors[input('Reflector: ').upper()]
-
-            rotors = l_rotor, m_rotor, r_rotor, reflector
+            rotors = [*rotor_inputs, reflector]
 
         return rotors
 
@@ -143,26 +159,26 @@ class enigma:
         if defaults:
             return plugboard
 
-        print('Input letter pairs for plugboard settings (A-Z).\n'
-              'Leave blank for defaults or to continue.')
+        print(
+            "Input letter pairs for plugboard settings (A-Z).\n"
+            "Leave blank for defaults or to continue."
+        )
 
         used_ltrs = []
         for count in range(1, 11):
             pair = []
             for i in range(2):
-                ltr = input('Enter a letter (pair %s): ' % count).upper()
+                ltr = input("Enter a letter (pair %s): " % count).upper()
                 while True:
-                    while ltr == '' and i == 1:
+                    while ltr == "" and i == 1:
                         print("Input can't be blank.")
-                        ltr = input('Enter a new letter (pair %s: '
-                                    % count).upper()
+                        ltr = input("Enter a new letter (pair %s: " % count).upper()
                     while ltr in used_ltrs:
-                        print('Letter already in use.')
-                        ltr = input('Enter a new letter (pair %s): '
-                                    % count).upper()
+                        print("Letter already in use.")
+                        ltr = input("Enter a new letter (pair %s): " % count).upper()
                     if ltr or i == 0:
-                        break   # second input can't be blank
-                if ltr == '':
+                        break  # second input can't be blank
+                if ltr == "":
                     break
                 used_ltrs += ltr
                 pair += ltr
@@ -188,7 +204,7 @@ class enigma:
         """
         return self.plugboard.get(keypress, keypress)
 
-    def rotor_encode(self, keypress):
+    def rotor_encode(self, keypress, show_rotors=False):
         """
         Runs text through the Enigma rotor mechanism
         """
@@ -202,8 +218,13 @@ class enigma:
         # Start position, ground = left, middle, right
         left, middle, right = self.ground[0], self.ground[1], self.ground[2]
 
-        print(self.ab_list[left], self.ab_list[middle],
-              self.ab_list[right])
+        if show_rotors:
+            print(
+                "Rotor positions:",
+                self.ab_list[left],
+                self.ab_list[middle],
+                self.ab_list[right],
+            )
 
         # Increment rotor position
         right += 1
@@ -211,12 +232,13 @@ class enigma:
             right = 0
         elif right == r_notch:
             middle += 1
+
         if middle > 25:
             middle = 0
-        elif middle == (m_notch - 1) and right == (
-                r_notch + 1):
+        elif middle == (m_notch - 1) and right == (r_notch + 1):
             middle += 1
             left += 1
+
         if left > 25:
             left = 0
 
@@ -242,41 +264,44 @@ class enigma:
 
         l_rotor_out = self.rotor_io(reflector_out, left, rf_l_rotor)
 
-        m_rotor_out = self.rotor_io(
-            l_rotor_out, middle - left, rf_m_rotor)
+        m_rotor_out = self.rotor_io(l_rotor_out, middle - left, rf_m_rotor)
 
-        r_rotor_out = self.rotor_io(
-            m_rotor_out, right - middle, rf_r_rotor)
+        r_rotor_out = self.rotor_io(m_rotor_out, right - middle, rf_r_rotor)
 
         rotor_encode_out = self.rotor_io(r_rotor_out, -right, self.ab_list)
         return rotor_encode_out
 
-    def ch_input(self):
+    def input(self):
         """
         Returns only valid input characters
         Valid inputs are non-accented upper-case characters A-Z
         """
-        keypress = input('Enter message: ').upper()
-        keypress = re.compile(r'[A-Z]').findall(keypress)
+        keypress = input("Enter message: ").upper()
+        keypress = re.compile(r"[A-Z]").findall(keypress)
         return keypress
 
-    def encode(self):
+    def encode(self, message):
         """
         Runs text through the Enigma machine,
         then outputs text in groups of four characters
         """
-        message = self.ch_input()
-        ch_list = ''
-        for ch in message:
+        ch_list = ""
+        show_rotors = False
+        for i, ch in enumerate(message):
+            if i == len(message) - 1:
+                show_rotors = True
             ch = self.plugboard_encode(ch)
-            ch = self.rotor_encode(ch)
+            ch = self.rotor_encode(ch, show_rotors)
             ch = self.plugboard_encode(ch)
             ch_list += ch
-        output = re.compile(r'[A-Z]{1,4}').findall(ch_list)
-        output = ' '.join(output)
+        output = re.compile(r"[A-Z]{1,4}").findall(ch_list)
+        output = " ".join(output)
         return output
 
 
 if __name__ == "__main__":
-    machine = enigma()
-    print(machine.encode())
+    machine = Enigma()
+    while True:
+        msg = machine.input()
+        msg = machine.encode(msg)
+        print("Encoded message:", msg)
